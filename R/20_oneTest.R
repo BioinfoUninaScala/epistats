e2f8486e9e8c6738cf1da36be76d7723670976ed
Antonella Sarnataro <sarnataro@bioalpha>
2021-08-10 17:19:39 +0200

    fourteenth commit

--- R/20_oneTest.R
+++ R/20_oneTest.R
@@ -1,6 +1,6 @@
 #' oneTest
 #'
-#' @importFrom purrr map reduce
+#' @importFrom purrr map reduce map_dbl
 #' @importFrom dplyr select full_join filter group_by summarize
 #' @param sample_list list of samples epiMatrices coming from epiAnalysis function
 #' @param region genomic coordinate of your region of interest
@@ -8,33 +8,42 @@
 #' @return Dataframe with statistics
 #' @export
 
-oneTest <- function(sample_list, region, metadata, rmUnmeth = FALSE, printData = FALSE){
+oneTest <- function(sample_list, region, metadata, rmUnmeth = FALSE, minGroups = 2, minSampleSize = 2, printData = FALSE){
   data <- getEpimatrix(sample_list, region)
   if(rmUnmeth == TRUE){
     data <- data[grep("1", colnames(data))]
     data = data %>% dplyr::filter(!rowSums(.) == 0)
   }
   dist = vegan::vegdist(data, method="bray")
+  groups = unique(metadata$Group)
   metadata <- metadata %>%
            dplyr::filter(Samples %in% rownames(data))
   check <- metadata %>%
               dplyr::group_by(Group) %>%
                  dplyr::summarize(count = length(Group))
-  check$reps <- paste(check$Group, check$count, sep = ":")
-  if(length(unique(metadata$Group)) > 1){
+  check <- check[check$count >= minSampleSize,]
+  metadata <- metadata[metadata$Group %in% check$Group,]
+  data <- data[rownames(data) %in% metadata$Samples,]
+  if(length(unique(metadata$Group)) >= minGroups){
+    a = groups %>% purrr::map_dbl(~ length(metadata$Samples[metadata$Group == .x]))
+    a = t(as.data.frame(a, row.names = groups))
+    rownames(a) = NULL
     ## problema formula
     p <- suppressMessages(adonis2(dist ~ Group, data = metadata))
-    result= data.frame("Region"= region,
+    result = data.frame("Region"= region,
                        "F.statistics"= p$F[1],
                        "p.value" = p$`Pr(>F)`[1],
-                       "num_EpiSpecies" = length(colnames(data)),
-                       "num_Reps" = paste(check$reps, collapse = ","))
+                       "num_EpiSpecies" = length(colnames(data)))
+    result = cbind(result, a)
   } else {
+    b = rep(NA, length(groups))
+    b = t(as.data.frame(b, row.names = groups))
+    rownames(b) = NULL
     result = data.frame("Region"= region,
                         "F.statistics"= NA,
                         "p.value" = NA,
-                        "num_EpiSpecies" = NA,
-                        "num_Reps" = NA)
+                        "num_EpiSpecies" = NA)
+    result = cbind(result, b)
   }
   if(printData == TRUE){
     print(data)
--- R/7_epiStat.R
+++ R/7_epiStat.R
@@ -1,6 +1,6 @@
 #' epiStat
 #'
-#' @importFrom purrr map reduce map_dfr
+#' @importFrom purrr map reduce map_dfr map_dbl
 #' @importFrom dplyr select distinct mutate full_join filter group_by summarize
 #' @importFrom future plan
 #' @importFrom furrr future_map2_dfr
@@ -14,7 +14,7 @@
 #' @export
 
 
-epiStat <- function(sample_list, metadata, rmUnmeth = FALSE, cores = 1){
+epiStat <- function(sample_list, metadata, rmUnmeth = FALSE, cores = 1, minGroups = 2, minSampleSize = 2){
   list = purrr::map(sample_list, function(x) x %>%
                       dplyr::select(id) %>%
                       dplyr::distinct() %>%
@@ -37,7 +37,7 @@ epiStat <- function(sample_list, metadata, rmUnmeth = FALSE, cores = 1){
   ## Remove warning message
   options(future.rng.onMisuse = "ignore")
   ## Call allStat for all the blocks
-  prova = furrr::future_map2_dfr(regs, filt_samples, allStat, metadata, rmUnmeth)
+  prova = furrr::future_map2_dfr(regs, filt_samples, allStat, metadata, rmUnmeth, minGroups, minSampleSize)
   ## Remove NAs
   prova = prova[stats::complete.cases(prova)==TRUE,]
   ## Adjust p-values
@@ -59,39 +59,47 @@ getEpimatrix <- function(samples, region){
   return(data)
 }
 
-oneStat <- function(sample_list, region, metadata, rmUnmeth){
+oneStat <- function(sample_list, region, metadata, rmUnmeth, minGroups, minSampleSize){
   data <- getEpimatrix(sample_list, region)
   if(rmUnmeth == TRUE){
     data <- data[grep("1", colnames(data))]
     data <- data %>% dplyr::filter(!rowSums(.) == 0)
     }
     dist = vegan::vegdist(data, method="bray")
-    metadata = metadata %>%
-      filter(Samples %in% rownames(data))
+    groups = unique(metadata$Group)
+    metadata <- metadata %>%
+      dplyr::filter(Samples %in% rownames(data))
     check <- metadata %>%
-               dplyr::group_by(Group) %>%
-                    dplyr::summarize(count = length(Group))
-    check$reps <- paste(check$Group, check$count, sep = ":")
-    if(length(unique(metadata$Group)) > 1){
-      ## problema formula
+      dplyr::group_by(Group) %>%
+      dplyr::summarize(count = length(Group))
+    check <- check[check$count >= minSampleSize,]
+    metadata <- metadata[metadata$Group %in% check$Group,]
+    data <- data[rownames(data) %in% metadata$Samples,]
+    if(length(unique(metadata$Group)) > minGroups){
+      a = groups %>% purrr::map_dbl(~ length(metadata$Samples[metadata$Group == .x]))
+      a = t(as.data.frame(a, row.names = groups))
+      rownames(a) = NULL
       p <- suppressMessages(adonis2(dist ~ Group, data = metadata))
-      result= data.frame("Region" = region,
+      result = data.frame("Region" = region,
                          "F.statistics" = p$F[1],
                          "p.value" = p$`Pr(>F)`[1],
-                         "num_EpiSpecies" = length(colnames(data)),
-                         "num_Reps" = paste(check$reps, collapse = ","))
+                         "num_EpiSpecies" = length(colnames(data)))
+      result = cbind(result, a)
     } else {
+      b = rep(NA, length(groups))
+      b = t(as.data.frame(b, row.names = groups))
+      rownames(b) = NULL
       result = data.frame("Region" = region,
                           "F.statistics" = NA,
                           "p.value" = NA,
-                          "num_EpiSpecies" = NA,
-                          "num_Reps" = NA)
+                          "num_EpiSpecies" = NA)
+      result = cbind(result,b)
     }
   return(result)
 }
 
 
-allStat <- function(regions, sample_list, metadata, rmUnmeth){
-  out = regions %>% map_dfr(~ oneStat(sample_list, .x, metadata, rmUnmeth))
+allStat <- function(regions, sample_list, metadata, rmUnmeth, minGroups, minSampleSize){
+  out = regions %>% map_dfr(~ oneStat(sample_list, .x, metadata, rmUnmeth, minGroups, minSampleSize))
   return(out)
 }
