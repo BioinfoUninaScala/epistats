4e4ebfe36129709107a60849b108dc7f68c93298
Aurorabrandi <adm2@hypatia>
2025-11-26 11:11:12 +0100

    document cdist_ont function

--- R/11_cdist.R
+++ R/11_cdist.R
@@ -10,6 +10,13 @@ cdist <- function(matrix){
   return(dist)
 }
 
+
+#' Calcutating the mean distance among the CpG sites contained in the analysed region for ONT.
+#'
+#' @param matrix Epiallele matrix for one genomic region
+#' @return Integer indicating the mean distance between CpG in the interval
+#' @export
+
 cdist_ont <- function(matrix){
   Cpos=as.numeric(colnames(matrix))
   dist=round(mean(Cpos[-1]-Cpos[-length(Cpos)]),2)
